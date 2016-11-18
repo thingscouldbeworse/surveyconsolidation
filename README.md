707f3fe1683254fd0d6346ca1dde7b5b72255850
Ethan Smith <ethansmith@EthanD-iMac.local>
2016-11-18 13:09:57 -0500

    changed pulldata to allow for multiple answer files

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 pulldata.py grabs all answer pdbs from the sdcards. Plug in as many as you want, run pulldata.py, replace sdcards, repeat.
 
-Once all answer pdbs are pulled run compile.py to enter them all into one csv
+Once all answer pdbs are pulled, run compile.py to enter them all into one csv
--- pulldata.py
+++ pulldata.py
@@ -9,18 +9,23 @@ import os
 from shutil import copyfile
 
 destination_prefix 	= 'files/' # this should be changed if the name of the destination directory is changed
-source_suffix 		= '/PALM/Launcher/SurveyAnswers.pdb' # change this if the SDcard directory structure changes
-volumes = os.listdir('/Volumes') # all attached drives
-sdcards = []
+source_suffix 		= 'AnswersArchive/' # change this if the SDcard directory structure changes
 
+sdcards = []
 search_card = 'COM' # all SDcards should be named 'COM0##'. If there's a different signifier, alter this line
+
+volumes = os.listdir('/Volumes') # all attached drives
+
 for volume in volumes:
 	if search_card in str(volume):
 		sdcards.append(volume) # append to list of actual sdcards
 
 for sdcard in sdcards: # for every actual sdcard
 
-	source 		= '/Volumes/' + str(sdcard) + source_suffix  # build the filepaths
-	destination = destination_prefix + str(sdcard) 
+	answerFiles = os.listdir( '/Volumes/' + str(sdcard) + '/' + source_suffix ) # answers are in multiple files 
+
+	for answerFile in answerFiles:
+		source 		= '/Volumes/' + str(sdcard) + '/' + source_suffix + str(answerFile) # build the filepaths
+		destination = destination_prefix + str(answerFile)
 	
-	copyfile( source, destination ) # copy the files where we need them
+		copyfile( source, destination ) # copy the files where we need them
